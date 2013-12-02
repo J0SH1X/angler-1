654c06e3ca53af9340ca74b8766267d933ff7dac
Johannes Weiner <hannes@cmpxchg.org>
2013-12-02 14:21:45 -0500

    mm + fs: store shadow entries in page cache
    Reclaim will be leaving shadow entries in the page cache radix tree
    upon evicting the real page.  As those pages are found from the LRU,
    an iput() can lead to the inode being freed concurrently.  At this
    point, reclaim must no longer install shadow pages because the inode
    freeing code needs to ensure the page tree is really empty.
    
    Add an address_space flag, AS_EXITING, that the inode freeing code
    sets under the tree lock before doing the final truncate.  Reclaim
    will check for this flag before installing shadow pages.
    
    Signed-off-by: Johannes Weiner <hannes@cmpxchg.org>

--- fs/ext4/inode.c
+++ fs/ext4/inode.c
@@ -222,8 +222,6 @@ void ext4_evict_inode(struct inode *inode)
 			filemap_write_and_wait(&inode->i_data);
 		}
 		truncate_inode_pages_final(&inode->i_data);
-
-		WARN_ON(atomic_read(&EXT4_I(inode)->i_ioend_count));
 		goto no_delete;
 	}
 
@@ -233,7 +231,8 @@ void ext4_evict_inode(struct inode *inode)
 
 	if (ext4_should_order_data(inode))
 		ext4_begin_ordered_truncate(inode, 0);
-	truncate_inode_pages_final(&inode->i_data);
+		truncate_inode_pages_final(&inode->i_data);
+
 
 	WARN_ON(atomic_read(&EXT4_I(inode)->i_ioend_count));
 
