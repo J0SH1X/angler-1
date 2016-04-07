6d779f53469c31fa8396e0396b8389df29629cd1
Alexei Starovoitov <ast@fb.com>
2016-04-06 18:43:23 -0700

    perf: remove unused __addr variable
    now all calls to perf_trace_buf_submit() pass 0 as 4th
    argument which will be repurposed in the next patch which will
    change the meaning of 1st arg of perf_tp_event() to event_type
    
    Signed-off-by: Alexei Starovoitov <ast@kernel.org>
    Acked-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Chatur27 <jasonbright2709@gmail.com>
    Change-Id: Ied0301233726ebb1a7d2f7faef36a18454ff648c

--- include/trace/ftrace.h
+++ include/trace/ftrace.h
@@ -709,7 +709,7 @@ perf_trace_##call(void *__data, proto)					\
 	struct ftrace_data_offsets_##call __maybe_unused __data_offsets;\
 	struct ftrace_raw_##call *entry;				\
 	struct pt_regs __regs;						\
-	u64 __addr = 0, __count = 1;					\
+	u64 __count = 1;					\
 	struct task_struct *__task = NULL;				\
 	struct hlist_head *head;					\
 	int __entry_size;						\
@@ -737,7 +737,7 @@ perf_trace_##call(void *__data, proto)					\
 	{ assign; }							\
 									\
 	head = this_cpu_ptr(event_call->perf_events);			\
-	perf_trace_buf_submit(entry, __entry_size, rctx, __addr,	\
+	perf_trace_buf_submit(entry, __entry_size, rctx, 0,	\
 		__count, &__regs, head, __task);			\
 }
 
