c056c7ffecc28eecbbc7385ca63f2f0548211e03
James Hogan <james.hogan@imgtec.com>
2024-06-16 19:40:30 +0300

    !fixup: asm-generic: Add renameat2 syscall

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -695,11 +695,10 @@ __SYSCALL(__NR_kcmp, sys_kcmp)
 __SYSCALL(__NR_finit_module, sys_finit_module)
 #define __NR_sched_setattr 274
 __SYSCALL(__NR_sched_setattr, sys_sched_setattr)
-#define __NR_sched_getattr 275
-__SYSCALL(__NR_sched_getattr, sys_sched_getattr)
-/* Backporting seccomp, stub out renameat2 */
+ * #define __NR_sched_getattr 275
+__SYSCALL(__NR_sched_getattr, sys_sched_getattr) */
 #define __NR_renameat2 276
-__SYSCALL(__NR_renameat2, sys_ni_syscall)
+__SYSCALL(__NR_renameat2, sys_renameat2)
 #define __NR_seccomp 277
 __SYSCALL(__NR_seccomp, sys_seccomp)
 #define __NR_getrandom 278
@@ -710,7 +709,7 @@ __SYSCALL(__NR_memfd_create, sys_memfd_create)
 __SYSCALL(__NR_bpf, sys_bpf)
 
 #undef __NR_syscalls
-#define __NR_syscalls 281
+#define __NR_syscalls 282
 
 /*
  * All syscalls below here should go away really,
