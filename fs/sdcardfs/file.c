e6e1ebde2d70b2a9fa3b5cdda2769a356aadb268
Yaroslav Zviezda <acroreiser@gmail.com>
2024-06-22 13:13:59 +0300

    fix kernel panics
    ik this is not a real fix but its "good enough" for now

--- fs/sdcardfs/file.c
+++ fs/sdcardfs/file.c
@@ -108,7 +108,10 @@ static long sdcardfs_unlocked_ioctl(struct file *file, unsigned int cmd,
 				  unsigned long arg)
 {
 	long err = -ENOTTY;
+	const struct cred *saved_cred = NULL;
 	struct file *lower_file;
+	struct dentry *dentry = file->f_path.dentry;
+	struct sdcardfs_sb_info *sbi = SDCARDFS_SB(dentry->d_sb);
 
 	lower_file = sdcardfs_lower_file(file);
 
@@ -141,6 +144,9 @@ static long sdcardfs_compat_ioctl(struct file *file, unsigned int cmd,
 {
 	long err = -ENOTTY;
 	struct file *lower_file;
+	const struct cred *saved_cred = NULL;
+	struct dentry *dentry = file->f_path.dentry;
+	struct sdcardfs_sb_info *sbi = SDCARDFS_SB(dentry->d_sb);
 
 	lower_file = sdcardfs_lower_file(file);
 
