2fd914e23e387077f78a8c7d2d1306c615fa7dc5
J0SH1X <aljoshua.hell@gmail.com>
2025-03-15 14:25:02 +0100

    Revert "locking/static_keys: Add static_key_{en,dis}able() helpers"
    This reverts commit 4791a1bc7221f7192909e39bf555f04e5857898f.

--- include/linux/jump_label.h
+++ include/linux/jump_label.h
@@ -215,9 +215,7 @@ static inline bool static_key_enabled(struct static_key *key)
 
 static inline void static_key_enable(struct static_key *key)
 {
-	int count = static_key_count(key);
-
-	WARN_ON_ONCE(count < 0 || count > 1);
+	int count = atomic_read(&key->enabled);
 
 	if (!count)
 		static_key_slow_inc(key);
@@ -225,9 +223,7 @@ static inline void static_key_enable(struct static_key *key)
 
 static inline void static_key_disable(struct static_key *key)
 {
-	int count = static_key_count(key);
-
-	WARN_ON_ONCE(count < 0 || count > 1);
+	int count = atomic_read(&key->enabled);
 
 	if (count)
 		static_key_slow_dec(key);
